e87e419e75125abf454198077c4d6acebc840fb5
bartosz.krawczyk <b.krawczyk@youweagency.com>
2023-07-02 19:30:48 +0200

    add a resolver for creation of new recommendations

---
+++ Model/Graphql/Resolver/CreateRecommendationList.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwiftOtter\FriendRecommendations\Model\Graphql\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class CreateRecommendationList implements ResolverInterface
+{
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        //TODO implement
+    }
+}
