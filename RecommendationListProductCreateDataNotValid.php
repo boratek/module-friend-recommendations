d2f1203837ff91f0b9d267b41c3585f1d4c9ddad
bartosz.krawczyk <b.krawczyk@youweagency.com>
2023-07-07 15:38:46 +0200

    add validation exception class

---
+++ RecommendationListProductCreateDataNotValid.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright Youwe. All rights reserved.
+ * https://www.youweagency.com
+ */
+
+declare(strict_types=1);
+
+namespace SwiftOtter\FriendRecommendations;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class RecommendationListProductCreateDataNotValid extends LocalizedException
+{
+
+}
